4a8562ad8b1d4dc481054e1ddffc13441fe29839
Ilya Sidorchik <ilya@sidorchik.ru>
2019-09-05 17:39:03 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Анимация. Крымский мост</title>
+        <link rel="stylesheet/less" type="text/css" href="less/styles.less">
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js"></script>
+    </head>
+    <body>
+        <div class="shock">
+            <div class="animated-video">
+                <div class="nebo">
+                    <div class="flag flag-ukraine"></div>
+                    <div class="flag flag-russian"></div>
+                </div>
+
+                <div class="traffic">
+                    <div class="car">
+                        <div class="body"></div>
+                        <div class="head"></div>
+                        <div class="wheel wheel-left"></div>
+                        <div class="wheel wheel-right"></div>
+                    </div>
+                </div>
+                <div class="bridge">
+                    <div class="road">
+                        <div>
+                            <span>К</span>
+                            <span>р</span>
+                            <span>ы</span>
+                            <span>м</span>
+                            <span>с</span>
+                            <span>к</span>
+                            <span>и</span>
+                            <span>й</span>
+                            <span>&thinsp;</span>
+                            <span>м</span>
+                            <span>о</span>
+                            <span>с</span>
+                            <span>т</span>
+                        </div>
+                    </div>
+                    <div class="stands"></div>
+                </div>
+                <div class="putin"></div>
+                <div class="sea"></div>
+            </div>
+        </div>
+    </body>
+</html>
